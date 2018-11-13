15f5609860e7f231535c90b334fae106a98cb6d7
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-11-13 17:25:28 +0000

    Created a two knex queries

---
+++ add_person.js
@@ -0,0 +1,22 @@
+var args = process.argv.slice(2);
+var userInput = [args[0], args[1], args[2]];
+
+const settings = require("./settings");
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+var pg = require('knex')({client: 'pg'});
+
+let userInput = { first_name: `${userInput[0]}`, last_name: `${userInput[1]}`, birthdate: `${userInput[2]}`};
+
+knex('famous_people')
+.insert(userInput).then(() => console.log("data inserted"))
+.catch((err) => { console.log( err); throw err })
+.finally(() => { knex.destroy();});
---
+++ lookup_people_knex.js
@@ -0,0 +1,23 @@
+var args = process.argv.slice(2);
+var userInput = args[0];
+
+const settings = require("./settings");
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+var pg = require('knex')({client: 'pg'});
+
+knex.select('*').from('famous_people')
+.where({first_name: `${userInput}`})
+.then((rows) => { for (row of rows)
+{ console.log(`${row['id']}: ${row['first_name']} ${row['last_name']} ${row['birthdate'] }`);}})
+.catch((err) => { console.log( err); throw err })
+.finally(() => { knex.destroy();});
